2598d96ce3fbc8882713d71efdfcafac4f958e56
levBagryansky <28lev11@gmail.com>
2024-09-21 23:03:24 +0300

    One more more

---
+++ 01/EmptyMain/main5.cpp
@@ -0,0 +1,4 @@
+#include <iostream>
+
+auto &ios = std::cout << "Hello world" << std::endl;
+int main() {}
--- 02/PrimeFactors/main.cpp
+++ 02/PrimeFactors/main.cpp
@@ -11,20 +11,31 @@ public:
         if (num_ < 2) {
             return -1;
         }
-        for (int i = max_prime_; i < num_; ++i) {
-
+        for (int i = curr_prime_; i <= num_; ++i) {
+            if (num_ % i == 0) {
+                curr_prime_ = i;
+                num_ /= curr_prime_;
+                return curr_prime_;
+            }
         }
+        assert(false); // Should not be here
+    }
+
+    bool notEmpty() {
+        return num_ != 1;
     }
 
 private:
     int num_;
-    int max_prime_{2};
+    int curr_prime_{2};
 };
 
 int main(int argc, char *argv[]) {
-    if (argc != 2)
-        abort();
-
+    FactorsGen gen{16};
+    while (gen.notEmpty()) {
+        std::cout << gen.next() << " ";
+    }
+    std::cout << std::endl;
     return 0;
 }
 
