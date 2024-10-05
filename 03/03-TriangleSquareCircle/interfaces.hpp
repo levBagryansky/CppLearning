aae2bdbd98e1eb9c10223123609674e4028f9741
levBagryansky <28lev11@gmail.com>
2024-10-05 18:49:23 +0300

    Castings

--- 03/03-TriangleSquareCircle/interfaces.hpp
+++ 03/03-TriangleSquareCircle/interfaces.hpp
@@ -7,12 +7,12 @@ class IVirtualDestructable {
 public: virtual ~IVirtualDestructable() = default;
 };
 
-class IPerimeter: public IVirtualDestructable {
-public: virtual double perimeter() = 0;
+class IPerimeter: public virtual IVirtualDestructable {
+public: virtual double perimeter() const = 0;
 };
 
-class IArea: public IVirtualDestructable {
-public: virtual double area() = 0;
+class IArea: public virtual IVirtualDestructable {
+public: virtual double area() const = 0;
 };
 
 class IShape: public IPerimeter, public IArea {};
