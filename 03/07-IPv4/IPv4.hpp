1bac35f3d658b855a125f48e431b51de18f0edce
levBagryansky <28lev11@gmail.com>
2024-10-12 14:07:09 +0300

    IPv4

---
+++ 03/07-IPv4/IPv4.hpp
@@ -0,0 +1,37 @@
+#pragma once
+
+class IPv4 {
+public:
+    IPv4(uint8_t first, uint8_t second, uint8_t third, uint8_t fourth):
+        first_(first), second_(second), third_(third), fourth_(fourth) {}
+
+public:
+    friend std::istream & operator>>(std::istream & istream, IPv4 & ipv4) {
+        char dot;
+        constexpr auto ERROR = "Correct format of IPv4: <u8>.<u8>.<u8>.<u8>";
+        uint first, second, third, fourth;
+        istream >> first >> dot;
+        if (dot != '.' || first > 255)
+            throw std::runtime_error(ERROR);
+        istream >> second >> dot;
+        if (dot != '.' || second > 255)
+            throw std::runtime_error(ERROR);
+        istream >> third >> dot;
+        if (dot != '.' || third > 255)
+            throw std::runtime_error(ERROR);
+        istream >> fourth;
+        if (fourth > 255)
+            throw std::runtime_error(ERROR);
+        ipv4 = IPv4(first, second, third, fourth);
+        return istream;
+    }
+
+    friend std::ostream & operator<<(std::ostream & ostream, const IPv4 & ipv4) {
+        constexpr char dot = '.';
+        ostream << static_cast<int>(ipv4.first_) << dot <<  static_cast<int>(ipv4.second_) << dot << static_cast<int>(ipv4.third_) << dot << static_cast<int>(ipv4.fourth_);
+        return ostream;
+    }
+
+private:
+    uint8_t first_, second_, third_, fourth_;
+};
---
+++ 03/07-IPv4/ipv4-test.cpp
@@ -0,0 +1,7 @@
+#include <gtest/gtest.h>
+
+#include "IPv4.hpp"
+
+TEST(IPv4, aboba) {
+    EXPECT_TRUE(1);
+}
